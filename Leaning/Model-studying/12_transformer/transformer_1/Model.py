d8bcbfe48db6b03166d9a130fb98c1f7215bc07e
廖博文 <38251055+LiaoBoWen@users.noreply.github.com>
2019-10-29 15:49:25 +0800

    Update Model.py

--- Leaning/Model-studying/12_transformer/transformer_1/Model.py
+++ Leaning/Model-studying/12_transformer/transformer_1/Model.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-# /usr/bin/python3
-'''
-Feb. 2019 by kyubyong park.
-kbpark.linguist@gmail.com.
-https://www.github.com/kyubyong/transformer
-
-Transformer network
-'''
-
-
 import tensorflow as tf
 
 from Data_process import load_vocab
